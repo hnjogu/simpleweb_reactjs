2f1252c44de568cfdd1b7960fb7fbac7619b5be6
harrugg <harrugg@localhost.localdomain>
2019-01-04 21:53:41 +0300

    new update

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,31 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import '../statics/custom.css';
 
 export default class CustomNavbar extends Component {
-	render() {
-		return (
-		    <Navbar default collapseOnSelect>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <Link to="/">Simpleweb</Link>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.collapse>
-                	<Nav pullRight>
-                	    <NavItem eventkey={1} componentClass={Link} to="/">
-                	       Home
-                        </NavItem>
-                        <NavItem eventkey={2} componentClass={Link} to="/about">
-                	       About
-                        </NavItem>
-                        <NavItem eventkey={3} componentClass={Link} to="/news">
-                	       News
-                        </NavItem>
-                	</Nav>
-                </Navbar.collapse>
-			</Navbar>			
-		);
-	}
+  render() {
+    return (
+      <Navbar default collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Best Photostars</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} componentClass={Link} href="/" to="/">
+              Home
+            </NavItem>
+            <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
+              About
+            </NavItem>
+            <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
+              News
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  }
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,37 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import { Jumbotron, Grid, Row, Col,Image, Button } from 'react-bootstrap';
-import './Home.css'
+import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import '../statics/custom.css';
 
 export default class Home extends Component {
-	render() {
-	    return (
-	        <Grid>
-	           <Jumbotron>
-	              <h2>Welcome to bestphotostars</h2>
-	              <p>This is my first reactjs code </p>
-	            </Jumbotron>
-	            <Link to="/about">
-	                <Button bsStyle="primary">About Us</Button>
-	            </Link>	       
-	        </Grid>
-	    )
-	}
+  render() {
+    return (
+      <Grid>
+        <Jumbotron>
+          <h2>Welcome to CodeLife</h2>
+          <p>This is how to build a website with React, React-Router & React-Bootstrap</p>
+          <Link to="/about">
+            <Button bsStyle="primary">Learn More</Button>
+          </Link>
+        </Jumbotron>
+        <Row className="show-grid text-center">
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="assets/person-1.jpg" circle className="profile-pic"/>
+            <h3>Code_0</h3>
+            <p>An example of python code.These little son of a guns hide in your brush and you just have to push them out</p>
+          </Col>
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="assets/person-2.jpg" circle className="profile-pic"/>
+            <h3>code_1</h3>
+            <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out.</p>
+          </Col>
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="assets/person-3.jpg" circle className="profile-pic"/>
+            <h3>Riff</h3>
+            <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out.</p>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
 }
